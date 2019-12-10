9c7ca7a3478c69156cbfb4651372b4f8de9d0a87
cyqsimon <28627918+cyqsimon@users.noreply.github.com>
2019-12-10 17:10:42 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name         Block 126 Ads
+// @namespace    https://scheimong.net/
+// @version      0.1
+// @description  Removes all ads (at least attempt to) from 126 mail
+// @author       Scheimong
+// @match        https://mail.126.com/*
+// @run-at       document-body
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var remove_by_class = ["gWel-recommend", "gWel-promt", "rd0", "Ab0"];
+    var remove_by_title = ["网易严选"];
+    var remove_by_selector = [];
+
+    const mutOb = new MutationObserver((mutList, ob) => {
+        for(let c of remove_by_class) {
+            let eList = document.getElementsByClassName(c);
+            tryRemoveAll(eList);
+        }
+
+        for(let t of remove_by_title) {
+            let eList = document.querySelectorAll(`[title=${t}]`);
+            tryRemoveAll(eList);
+        }
+
+        for(let s of remove_by_selector) {
+            let eList = document.querySelectorAll(s);
+            tryRemoveAll(eList);
+        }
+    });
+
+    mutOb.observe(document, {childList: true, subtree: true});
+
+    function tryRemoveAll(eList) {
+        if(eList.length == 0) { // Not yet in DOM
+            return false;
+        }
+        else { // Remove
+            for(let e of eList) {
+                e.remove();
+                console.log(`Known ad removed! ${e}`);
+            }
+            return true;
+        }
+    }
+})();
