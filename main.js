8eb12c601865de56d9a52abec802386e2f6a6e1d
cyqsimon <28627918+cyqsimon@users.noreply.github.com>
2020-12-22 14:40:22 +0800

    Code format & refactor

--- main.js
+++ main.js
@@ -13,36 +13,52 @@
 
 "use strict";
 
-(function() {
-    const remove_by_class = ["gWel-recommend", "gWel-promt", "mailgg", "rd0", "Ab0", "qJ0", "xd0", "tN0", "mailApp", "xH1", "tA1"];
-    const remove_by_title = ["网易严选", "携程旅行", "半个电台"];
-    const remove_by_selector = [];
+(function () {
+    const removeByClass = [
+        "gWel-recommend",
+        "gWel-promt",
+        "mailgg",
+        "rd0",
+        "Ab0",
+        "qJ0",
+        "xd0",
+        "tN0",
+        "mailApp",
+        "xH1",
+        "tA1",
+    ];
+    const removeByTitle = [
+        "网易严选",
+        "携程旅行",
+        "半个电台",
+    ];
+    const removeBySelector = [];
 
     const mutOb = new MutationObserver((mutList, ob) => {
-        for(let c of remove_by_class) {
+        for (let c of removeByClass) {
             let eList = document.querySelectorAll(`.${c}`);
             tryRemoveAll(eList);
         }
 
-        for(let t of remove_by_title) {
+        for (let t of removeByTitle) {
             let eList = document.querySelectorAll(`[title=${t}]`);
             tryRemoveAll(eList);
         }
 
-        for(let s of remove_by_selector) {
+        for (let s of removeBySelector) {
             let eList = document.querySelectorAll(s);
             tryRemoveAll(eList);
         }
     });
 
-    mutOb.observe(document, {childList: true, subtree: true});
+    mutOb.observe(document, { childList: true, subtree: true });
 
     function tryRemoveAll(eList) {
-        if(eList.length == 0) { // Not yet in DOM
+        if (eList.length == 0) { // Not yet in DOM
             return false;
         }
         else { // Remove
-            for(let e of eList) {
+            for (let e of eList) {
                 e.remove();
                 console.log("Known ad removed!", e);
             }
