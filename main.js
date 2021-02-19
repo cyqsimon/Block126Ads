6bc6cb733082286f86440ec85f3ccc99f382bbbd
cyqsimon <28627918+cyqsimon@users.noreply.github.com>
2021-02-19 09:28:39 +0800

    Refactoring

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Block 126 Ads
 // @namespace    https://scheimong.net/
-// @version      0.1.10
+// @version      0.1.11
 // @updateURL    https://raw.githubusercontent.com/cyqsimon/Block126Ads/master/main.js
 // @downloadURL  https://raw.githubusercontent.com/cyqsimon/Block126Ads/master/main.js
 // @description  Removes all ads (at least attempt to) from 126 mail
@@ -14,55 +14,38 @@
 "use strict";
 
 (function () {
-    const removeByClass = [
-        "gWel-recommend",
-        "gWel-promt",
-        "mailgg",
-        "rd0",
-        "Ab0",
-        "qJ0",
-        "xd0",
-        "tN0",
-        "mailApp",
-        "xH1",
-        "tA1",
+    const removeList = [
+        // class
+        ".gWel-recommend",
+        ".gWel-promt",
+        ".mailgg",
+        ".rd0",
+        ".Ab0",
+        ".qJ0",
+        ".xd0",
+        ".tN0",
+        ".mailApp",
+        ".xH1",
+        ".tA1",
+        // title
+        "[title=网易严选]",
+        "[title=携程旅行]",
+        "[title=半个电台]",
     ];
-    const removeByTitle = [
-        "网易严选",
-        "携程旅行",
-        "半个电台",
-    ];
-    const removeBySelector = [];
 
     const mutOb = new MutationObserver((mutList, ob) => {
-        for (let c of removeByClass) {
-            let eList = document.querySelectorAll(`.${c}`);
-            tryRemoveAll(eList);
-        }
-
-        for (let t of removeByTitle) {
-            let eList = document.querySelectorAll(`[title=${t}]`);
-            tryRemoveAll(eList);
-        }
-
-        for (let s of removeBySelector) {
+        for (let s of removeList) {
             let eList = document.querySelectorAll(s);
-            tryRemoveAll(eList);
+            removeAll(eList);
         }
     });
 
     mutOb.observe(document, { childList: true, subtree: true });
 
-    function tryRemoveAll(eList) {
-        if (eList.length == 0) { // Not yet in DOM
-            return false;
-        }
-        else { // Remove
-            for (let e of eList) {
-                e.remove();
-                console.log("Known ad removed!", e);
-            }
-            return true;
+    function removeAll(eList) {
+        for (let e of eList) {
+            e.remove();
+            console.log("Known ad removed!", e);
         }
     }
 })();
